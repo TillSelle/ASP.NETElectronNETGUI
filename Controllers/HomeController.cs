538230049b44db27fd554c158a265c1f9668994f
Till Selle <97885878+TillSelle@users.noreply.github.com>
2022-02-25 22:55:30 +0100

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using Electron_Client.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Electron_Client.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index() =>
+            View();
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Latest()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
